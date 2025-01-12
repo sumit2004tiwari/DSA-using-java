375b453c082fb2eb7b6af81624835be7b331a75a
sumit2004tiwari <sumit2004tiwari@gmail.com>
2025-01-12 22:10:26 +0530

    number pyramid

---
+++ src/numerPyramid.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class numerPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+
+        for (int i=1; i<row; i++){
+            for (int j=1; j<row-i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 1; k<=i; k++){
+                System.out.print(k);
+            }
+            for (int l = i-1; l>=1 ; l-- ){
+                System.out.print(l);
+            }
+            System.out.println();
+        }
+    }
+}
