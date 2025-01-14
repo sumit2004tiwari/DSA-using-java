00d06f7c651c08c7afeb5b86c727a03d04f16b15
sumit2004tiwari <sumit2004tiwari@gmail.com>
2025-01-14 21:55:47 +0530

    hollow pyramid number

---
+++ src/hollownumbertriangle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class hollownumbertriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+
+        for (int i =1 ; i<=row; i++){
+           for (int j=1; j<=row-i; j++){
+               System.out.print(" ");
+           }
+           for (int k=1 ; k<= 2*i-1 ; k++){
+
+               if (k == 1 || k == 2 * i - 1 || i == row) {
+                   System.out.print(i);
+               } else {
+                   System.out.print(" ");
+               }           }
+            System.out.println();
+        }
+    }
+}
---
+++ src/trianglenumberpyramid.java
@@ -0,0 +1,5 @@
+public class trianglenumberpyramid {
+    public static void main(String[] args) {
+
+    }
+}
