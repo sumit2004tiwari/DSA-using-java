21d02d6d5177b8e7d9430ec14f0a482e252bf2f5
sumit2004tiwari <sumit2004tiwari@gmail.com>
2025-01-14 22:16:35 +0530

    binary to decimal

---
+++ src/binarytodecimal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class binarytodecimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int binarynumber = sc.nextInt(); // 1001
+
+        int ans = 0;
+        int power =1;
+        while (binarynumber > 0){
+           ans +=  (binarynumber % 10) * power;
+           binarynumber = binarynumber /10;
+           power = power *2;
+
+        }
+        System.out.println(ans + "ans");
+    }
+}
--- src/hollownumbertriangle.java
+++ src/hollownumbertriangle.java
@@ -7,7 +7,7 @@ public class hollownumbertriangle {
 
         for (int i =1 ; i<=row; i++){
            for (int j=1; j<=row-i; j++){
-               System.out.print(" ");
+               System.out.print("");
            }
            for (int k=1 ; k<= 2*i-1 ; k++){
 
